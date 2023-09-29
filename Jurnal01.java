f7e412fdfde702e1325dea2357975f672d73b3bc
azrifauzansofyan <azrifauzan50@gmail.com>
2023-09-29 14:58:43 +0700

    first commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -3,6 +3,7 @@ public class Jurnal01{
         System.out.println("Nama: Azri Fauzan Sofyan");
         System.out.println("Nim: 607062300053");
         System.out.println("Kelas: D3IF-47-01");
+        
 
     }
 }
